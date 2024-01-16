b169af52ca1f9ed7d893385757dad1249f2d97cd
roman2702rb <roman2702rb@gmail.com>
2024-01-16 19:00:56 +0200

    detailed frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To Alternatif Scents</h1>
+            <Outlet />
+        </Container>
+        <Footer />
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Navbar, Nav, Container} from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/logo-light.png'
+import { LinkContainer} from 'react-router-bootstrap'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand  > 
+              <img src={logo} alt="Alternatif" className='logo'/>
+            </Navbar.Brand>
+          </LinkContainer>
+          
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse>
+            <Nav className='ms-auto'>
+              <LinkContainer to="/cart" >
+                <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="login">
+                <Nav.Link href="/login"><FaUser/> Sign In</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
