0b2daf2d72e8c80a8b9d20dd6474cc439914f4b6
Roman Blumovich <roman.blumovich@gmail.com>
2024-01-29 22:08:42 +0200

    prepare for production

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ app.use(express.urlencoded({ extended: true}))
 // cookie-parser middleware
 app.use(cookieParser())
 
-app.get('/', (req,res) => {
-    res.send("API is running")
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -35,6 +32,20 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+if(process.env.NODE_ENV === 'production'){
+    // set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    // any route not api would be directed to index.js
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html' ))
+    )
+
+}else{
+    app.get('/', (req,res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
